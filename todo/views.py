97f625e86a5fd16dcdb480a4a15ae9f92fefb398
SAMEER SALIM <sameersalim@SAMEERs-MacBook-Pro.local>
2025-06-10 17:58:30 +0530

    mark as done functonality

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import Task
 
@@ -7,3 +7,9 @@ def addTask(request):
     task=request.POST['task'] # Debugging line to check POST data 
     Task.objects.create(task=task)
     return redirect('home')  # Placeholder response for task addition
+
+def mark_as_done(request,pk):
+    task=get_object_or_404(Task, pk=pk)
+    task.is_completed = True
+    task.save()
+    return redirect('home')  # Redirect to home after marking task as done
